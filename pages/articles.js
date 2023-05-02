bae4b765d3271c6aed47184600b928a7ef733a69
Dan <daysprng@gmail.com>
2023-05-02 23:34:09 +0300

    search (case sensitive in cyrillic)

--- pages/articles.js
+++ pages/articles.js
@@ -1,18 +1,26 @@
 import {useState, useEffect} from 'react'
+import {useRouter} from 'next/router'
 import MainContainer from "../components/MainContainer";
 import Preview from '../components/Preview';
 import Island from '../components/UI/Island';
 import styles from'../styles/articles.module.scss';
 
 const Articles = ({articles}) => {
-    const [filter, setFilter] = useState('1');
+    const [search, setSearch] = useState('');
+    const router = useRouter();
+    const handleSearch = () => {
+        console.log(encodeURI(search))
+        router.replace({
+            query: { ...router.query, search: encodeURI(search) },
+         });
+    }
 
     return (
         <MainContainer keywords={"articles next js"}>
             <Island>
                 <div className={styles.search}>
-                    <input type="text" placeholder="Поиск" />
-                    <button>Найти</button>
+                    <input value={search} onChange={e => setSearch(e.target.value)} type="search" name="searchbar" placeholder="Поиск" />
+                    <button onClick={handleSearch}>Найти</button>
                 </div>
             </Island>
             <Island>
@@ -33,7 +41,9 @@ const Articles = ({articles}) => {
 export default Articles;
 
 export const getServerSideProps = async (context) => {
-    const response = await fetch(`http://142.132.182.231:1337/api/articles?populate=*&sort=views%3Adesc`)
+    const filter = context.query.search ? `&filters[Title][$contains]=${context.query.search}` : '';
+    console.log(filter);
+    const response = await fetch(`http://142.132.182.231:1337/api/articles?populate=*${filter}&sort=views%3Adesc`)
     const json = await response.json();
     const articles = json.data;
 
