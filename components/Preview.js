c435a70a6989636cf4eb2b14f12ad956b8a1f0c8
Dan <daysprng@gmail.com>
2023-05-16 18:26:21 +0300

    add pagination

--- components/Preview.js
+++ components/Preview.js
@@ -2,17 +2,21 @@ import React from 'react';
 import classes from'../styles/Preview.module.scss';
 import A from "./A";
 
+
 const Preview = ({product}) => {
     return (
         <div className={classes.product}>
                             <img
                                 className={classes.thumb}
-                                src={process.env.UPLOADS_URL+product.attributes.cover?.data?.attributes?.formats?.thumbnail?.url}
+                                // src={process.env.UPLOADS_URL+product.attributes.cover?.data?.attributes?.formats?.thumbnail?.url}
+                                src={product.attributes.cover.data !== null ? process.env.UPLOADS_URL+product.attributes.cover.data.attributes.formats.thumbnail.url : "/meter_placeholder.svg"}
                             />
                             <div className={classes.info}>
                                 <h3 className={classes.title}>
                                     <A href={`/articles/${product.id}`} text={product.attributes.title} />
+                                    
                                 </h3>
+                                <p>{product.attributes.ngr}</p>
                                 <p>views: {product.attributes.views}</p>
                             </div>
                         </div>
--- pages/articles.js
+++ pages/articles.js
@@ -6,29 +6,53 @@ import Preview from '../components/Preview';
 import Island from '../components/UI/Island';
 import classes from'../styles/articles.module.scss';
 
-const Articles = ({articles}) => {
+const Articles = ({articles, meta}) => {
     const [search, setSearch] = useState('');
     const router = useRouter();
-    const handleSearch = () => {
+    const handleSearch = (event) => {
+        event.preventDefault();
+        console.log(encodeURI(search))
         router.replace({
-            query: { ...router.query, search: encodeURI(search) },
+            query: { ...router.query, search: encodeURI(search), page: 1 },
+         });
+    }
+    const handlePagination = (destinaionPage) => {
+        router.replace({
+            query: { ...router.query, page: destinaionPage },
          });
     }
 
     return (
         <MainContainer keywords={"articles next js"}>
             <Island>
-                <div className={classes.search}>
-                    <input value={search} onChange={e => setSearch(e.target.value)} type="search" name="searchbar" placeholder="Поиск" />
-                    <button onClick={handleSearch}>Найти</button>
+            <div >
+                    <form id="search" className={classes.search} onSubmit={handleSearch}>
+                        <input value={search} onChange={e => setSearch(e.target.value)} type="search" name="searchbar" placeholder="Поиск" />
+                        <button type="submit">Найти</button>
+                    </form>
                 </div>
             </Island>
             <Island>
-                <h1 className={classes.title}>Счетчики</h1>
+                <h1 className={classes.title}>Счетчики:  {meta.pagination.total}</h1>
                 <div className={classes.articles}>
                     {articles.map(article =>
                         <Preview key={article.id} product={article} />
                     )}
+                </div>
+                <div>
+                    <p>Страница: {meta.pagination.page} из {meta.pagination.pageCount}</p>
+                    <button
+                        onClick={() => handlePagination(meta.pagination.page - 1)}
+                        disabled={meta.pagination.page === 1}
+                    >
+                        Предыдущая
+                    </button>
+                    <button
+                        onClick={() => handlePagination(meta.pagination.page + 1)}
+                        disabled={meta.pagination.page === meta.pagination.pageCount}
+                    >
+                        Следующая
+                    </button>
                 </div>
             </Island>
         </MainContainer>
@@ -38,11 +62,13 @@ const Articles = ({articles}) => {
 export default Articles;
 
 export const getServerSideProps = async (context) => {
+    const page = context.query.page ? context.query.page : 1;
     const filter = context.query.search ? `&filters[Title][$contains]=${context.query.search}` : '';
-    const response = await fetcher(`/articles?populate=*${filter}&sort=views%3Adesc`)
+    const response = await fetcher(`/articles?populate=*${filter}&sort=views%3Adesc&pagination[page]=${page}`)
     const articles = response.data.data;
+    const meta = response.data.meta
 
     return {
-        props: {articles},
+        props: {articles, meta},
     }
 }
